15ed0737936e3bcca6e6844b8fab774e0eb99e03
najeesimmons <najee.bryant.simmons@gmail.com>
2022-06-23 14:35:28 -0400

    added css module to MealItems and rendering MealItems in Available meals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,5 @@
 import Card from "../UI/Card";
+import MealItem from "./MealItem.js/MealItem";
 import classes from "./AvailableMeals.module.css";
 
 const DUMMY_MEALS = [
@@ -30,7 +31,12 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
-    <li key={meal.name}>{meal.name}</li>
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
   ));
 
   return (
